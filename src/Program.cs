c69cdf9a868309d1123242b9129535e5efa586f5
Max <rendon4ik@gmail.com>
2022-02-02 20:26:51 +0200

    Init project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multi_Paradigm_Programming
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
