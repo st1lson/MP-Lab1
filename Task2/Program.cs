5d38b844a15a019a9ec99fdc835ed00640eb25a2
Max <rendon4ik@gmail.com>
2022-02-04 11:51:15 +0200

    Init second task

---
+++ Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace Task2
+{
+    internal class Program
+    {
+        private static readonly char[] Separators = { ' ', '\r', '\n', '\t' };
+
+        private static void Main()
+        {
+            string[] data = new string[10];
+            using (StreamReader reader = new("source.txt"))
+            {
+                string str = string.Empty;
+                int dataIndex = 0;
+                forReadStatement:
+                char dataChar = (char)reader.Read();
+                int checkIndex = 0;
+                forCheckStatement:
+                if (dataChar == Separators[checkIndex])
+                {
+                    data[dataIndex++] = str;
+                    str = default;
+                    goto forReadStatement;
+                }
+
+                checkIndex++;
+                if (checkIndex < Separators.Length)
+                {
+                    goto forCheckStatement;
+                }
+
+                str += dataChar;
+                if (reader.EndOfStream)
+                {
+                    data[dataIndex++] = str;
+                }
+
+                if (data.Length * 0.8 <= dataIndex)
+                {
+                    string[] newArray = new string[data.Length * 2];
+                    int copyIndex = 0;
+                    forCopyDataStatement:
+                    newArray[copyIndex] = data[copyIndex];
+                    copyIndex++;
+
+                    if (copyIndex < dataIndex)
+                    {
+                        goto forCopyDataStatement;
+                    }
+
+                    data = newArray;
+                }
+
+                if (!reader.EndOfStream)
+                {
+                    goto forReadStatement;
+                }
+            }
+        }
+    }
+}
