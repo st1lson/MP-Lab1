36c032e834644a09be78365e41cad8a30094ba79
Max <rendon4ik@gmail.com>
2022-02-05 14:36:37 +0200

    Add output for second task

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 
 namespace Task2
 {
@@ -12,128 +10,196 @@ namespace Task2
             (string, int[], int)[] words = new (string, int[], int)[10];
             int lineIndex = 0;
             int elementsCount = 0;
-            forStatement:
-                string line = data[lineIndex];
-                if (line == "")
-                {
-                    goto forStatementEnd;
-                }
-
-                int charIndex = 0;
-                string str = string.Empty;
-                forReadLineStatement:
-                    char wordChar = line[charIndex];
-                    if (wordChar is >= 'A' and <= 'Z')
-                    {
-                        wordChar = (char)(wordChar + 32);
-                        goto addChar;
-                    }
-
-                    if (wordChar is >= 'a' and <= 'z')
-                    {
-                        goto addChar;
-                    }
-
-                    if (str is null || str.Length == 0)
-                    {
-                        goto addCharEnd;
-                    }
-
-                    int wordsCheckIndex = 0;
-                    checkWords:
-                        if (words[wordsCheckIndex].Item1 == str)
-                        {
-                            int count = words[wordsCheckIndex].Item3;
-                            if (count > 100)
-                            {
-                                words[wordsCheckIndex].Item3++;
-                                goto checkWordsEnd;
-                            }
-
-                            words[wordsCheckIndex].Item2[count - 1] = lineIndex / 45 + 1;
-                            words[wordsCheckIndex].Item3++;
-                            goto checkWordsEnd;
-                        }
-
-                        wordsCheckIndex++;
-                        if (wordsCheckIndex < elementsCount)
-                        {
-                            goto checkWords;
-                        }
-
-                        if (words.Length * 0.8 <= elementsCount)
-                        {
-                            (string, int[], int)[] newArray = new (string, int[], int)[words.Length * 2];
-                            int copyIndex = 0;
-                            forCopyDataStatement:
-                                newArray[copyIndex] = words[copyIndex];
-                                copyIndex++;
-
-                                if (copyIndex < elementsCount)
-                                {
-                                    goto forCopyDataStatement;
-                                }
-
-                            words = newArray;
-                        }
-
-                        int[] pages = new int[100];
-                        pages[0] = lineIndex / 45 + 1;
-                        (string, int[], int) word = (str, pages, 1);
-                        words[elementsCount++] = word;
-
-                    checkWordsEnd:
-                        str = default;
-                        goto addCharEnd;
-
-                    addChar:
-                        str += wordChar;
-
-                    addCharEnd:
-
-                    charIndex++;
-                    if (charIndex < line.Length)
-                    {
-                        goto forReadLineStatement;
-                    }
-
-                    forStatementEnd:
-                    lineIndex++;
-                    if (lineIndex < data.Length)
-                    {
-                        goto forStatement;
-                    }
-
-                    int wordIndex = 0;
-            removeExtraOccurrences:
-                if (words[wordIndex].Item3 > 100)
-                {
-                    words[wordIndex] = default;
-                }
-
-                wordIndex++;
-                if (wordIndex < elementsCount)
-                {
-                    goto removeExtraOccurrences;
-                }
-
-            // sort here
-
-            foreach (var valueTuple in words.OrderBy(w => w.Item1))
+        forStatement:
+            string line = data[lineIndex];
+            if (line == "")
             {
-                if (valueTuple.Item3 > 0)
-                {
-                    Console.Write($"{valueTuple.Item1} - ");
-                    foreach (var k in valueTuple.Item2)
-                    {
-                        if (k != 0)
-                        {
-                            Console.Write($"{k}, ");
-                        }
-                    }
+                goto forStatementEnd;
+            }
 
-                    Console.Write("\n");
+            int charIndex = 0;
+            string str = string.Empty;
+        forReadLineStatement:
+            char wordChar = line[charIndex];
+            if (wordChar is >= 'A' and <= 'Z')
+            {
+                wordChar = (char)(wordChar + 32);
+                goto addChar;
+            }
+
+            if (wordChar is >= 'a' and <= 'z')
+            {
+                goto addChar;
+            }
+
+            if (str is null || str.Length == 0)
+            {
+                goto addCharEnd;
+            }
+
+            int wordsCheckIndex = 0;
+        checkWords:
+            if (words[wordsCheckIndex].Item1 == str)
+            {
+                int count = words[wordsCheckIndex].Item3;
+                if (count > 100)
+                {
+                    words[wordsCheckIndex].Item3++;
+                    goto checkWordsEnd;
                 }
+
+                words[wordsCheckIndex].Item2[count - 1] = lineIndex / 45 + 1;
+                words[wordsCheckIndex].Item3++;
+                goto checkWordsEnd;
+            }
+
+            wordsCheckIndex++;
+            if (wordsCheckIndex < elementsCount)
+            {
+                goto checkWords;
+            }
+
+            if (words.Length * 0.8 <= elementsCount)
+            {
+                (string, int[], int)[] newArray = new (string, int[], int)[words.Length * 2];
+                int copyIndex = 0;
+            forCopyDataStatement:
+                newArray[copyIndex] = words[copyIndex];
+                copyIndex++;
+
+                if (copyIndex < elementsCount)
+                {
+                    goto forCopyDataStatement;
+                }
+
+                words = newArray;
+            }
+
+            int[] pages = new int[100];
+            pages[0] = lineIndex / 45 + 1;
+            (string, int[], int) word = (str, pages, 1);
+            words[elementsCount++] = word;
+
+        checkWordsEnd:
+            str = default;
+            goto addCharEnd;
+
+        addChar:
+            str += wordChar;
+
+        addCharEnd:
+
+            charIndex++;
+            if (charIndex < line.Length)
+            {
+                goto forReadLineStatement;
+            }
+
+        forStatementEnd:
+            lineIndex++;
+            if (lineIndex < data.Length)
+            {
+                goto forStatement;
+            }
+
+            int wordIndex = 0;
+        removeExtraOccurrences:
+            if (words[wordIndex].Item3 > 100)
+            {
+                words[wordIndex] = default;
+            }
+
+            wordIndex++;
+            if (wordIndex < elementsCount)
+            {
+                goto removeExtraOccurrences;
+            }
+
+            int i = 0;
+        firstSortStatement:
+            int j = 0;
+        secondSortStatement:
+            bool needSwap = false;
+            string firstWord = words[j].Item1;
+            string secondWord = words[j + 1].Item1;
+            if (secondWord is null)
+            {
+                goto compareWordsEnd;
+            }
+
+            if (firstWord is null)
+            {
+                needSwap = true;
+                goto compareWordsEnd;
+            }
+
+            int length = secondWord.Length < firstWord.Length ? secondWord.Length : firstWord.Length;
+            int wordCharIndex = 0;
+        compareWords:
+            char firstWordChar = firstWord[wordCharIndex];
+            char secondWordChar = secondWord[wordCharIndex];
+
+            if (firstWordChar > secondWordChar)
+            {
+                needSwap = true;
+                goto compareWordsEnd;
+            }
+
+            if (firstWordChar < secondWordChar)
+            {
+                goto compareWordsEnd;
+            }
+
+            wordCharIndex++;
+            if (wordCharIndex < length)
+            {
+                goto compareWords;
+            }
+
+        compareWordsEnd:
+            if (needSwap)
+            {
+                (words[j], words[j + 1]) = (words[j + 1], words[j]);
+            }
+
+            j++;
+            if (j < elementsCount - 1)
+            {
+                goto secondSortStatement;
+            }
+
+            i++;
+            if (i < elementsCount)
+            {
+                goto firstSortStatement;
+            }
+
+            using StreamWriter writer = new("output.txt");
+            int outputWordIndex = 0;
+        forOutputStatement:
+            if (words[outputWordIndex].Item3 > 0)
+            {
+                writer.Write($"{words[outputWordIndex].Item1} - ");
+                int pageIndex = 0;
+            forOutputPagesStatement:
+                if (words[outputWordIndex].Item2[pageIndex] != 0)
+                {
+                    writer.Write($"{words[outputWordIndex].Item2[pageIndex]}, ");
+                }
+
+                pageIndex++;
+                if (pageIndex < words[outputWordIndex].Item2.Length)
+                {
+                    goto forOutputPagesStatement;
+                }
+
+                writer.Write('\n');
+            }
+
+            outputWordIndex++;
+            if (outputWordIndex < elementsCount)
+            {
+                goto forOutputStatement;
             }
         }
     }
--- src/Program.cs
+++ src/Program.cs
@@ -15,180 +15,180 @@ namespace Multi_Paradigm_Programming
             {
                 string str = string.Empty;
                 int dataIndex = 0;
-                forReadStatement:
-                    char dataChar = (char)reader.Read();
-                    int checkIndex = 0;
-                    forCheckStatement:
-                        if (dataChar == Separators[checkIndex])
-                        {
-                            data[dataIndex++] = str;
-                            str = default;
-                            goto forReadStatement;
-                        }
+            forReadStatement:
+                char dataChar = (char)reader.Read();
+                int checkIndex = 0;
+            forCheckStatement:
+                if (dataChar == Separators[checkIndex])
+                {
+                    data[dataIndex++] = str;
+                    str = default;
+                    goto forReadStatement;
+                }
 
-                        checkIndex++;
-                        if (checkIndex < Separators.Length)
-                        {
-                            goto forCheckStatement;
-                        }
+                checkIndex++;
+                if (checkIndex < Separators.Length)
+                {
+                    goto forCheckStatement;
+                }
 
-                    str += dataChar;
-                    if (reader.EndOfStream)
+                str += dataChar;
+                if (reader.EndOfStream)
+                {
+                    data[dataIndex++] = str;
+                }
+
+                if (data.Length * 0.8 <= dataIndex)
+                {
+                    string[] newArray = new string[data.Length * 2];
+                    int copyIndex = 0;
+                forCopyDataStatement:
+                    newArray[copyIndex] = data[copyIndex];
+                    copyIndex++;
+
+                    if (copyIndex < dataIndex)
                     {
-                        data[dataIndex++] = str;
+                        goto forCopyDataStatement;
                     }
 
-                    if (data.Length * 0.8 <= dataIndex)
-                    {
-                        string[] newArray = new string[data.Length * 2];
-                        int copyIndex = 0;
-                        forCopyDataStatement:
-                            newArray[copyIndex] = data[copyIndex];
-                            copyIndex++;
+                    data = newArray;
+                }
 
-                            if (copyIndex < dataIndex)
-                            {
-                                goto forCopyDataStatement;
-                            }
-
-                        data = newArray;
-                    }
-
-                    if (!reader.EndOfStream)
-                    {
-                        goto forReadStatement;
-                    }
+                if (!reader.EndOfStream)
+                {
+                    goto forReadStatement;
+                }
             }
 
             (string, int)[] distinctWords = new (string, int)[1];
             int index = 0;
             int elementsCount = 0;
-            forStatement:
-                string word = data[index];
-                if (word is null)
+        forStatement:
+            string word = data[index];
+            if (word is null)
+            {
+                index++;
+                if (index == data.Length - 1)
                 {
-                    index++;
-                    if (index == data.Length - 1)
-                    {
-                        goto forStatementEnd;
-                    }
-
-                    goto forStatement;
+                    goto forStatementEnd;
                 }
 
-                if (distinctWords.Length * 0.8 <= elementsCount)
-                {
-                    (string, int)[] newArray = new (string, int)[distinctWords.Length * 2];
-                    int copyIndex = 0;
-                    forCopyStatement:
-                        newArray[copyIndex] = distinctWords[copyIndex];
-                        copyIndex++;
-                    
-                        if (copyIndex < elementsCount)
-                        {
-                            goto forCopyStatement;
-                        }
+                goto forStatement;
+            }
 
-                    distinctWords = newArray;
+            if (distinctWords.Length * 0.8 <= elementsCount)
+            {
+                (string, int)[] newArray = new (string, int)[distinctWords.Length * 2];
+                int copyIndex = 0;
+            forCopyStatement:
+                newArray[copyIndex] = distinctWords[copyIndex];
+                copyIndex++;
+
+                if (copyIndex < elementsCount)
+                {
+                    goto forCopyStatement;
                 }
 
-                int charIndex = 0;
-                StringBuilder lowerCaseWordBuilder = new()
-                {
-                    Length = word.Length
-                };
-                forWordStatement:
-                    if (word[charIndex] >= 'A' && word[charIndex] <= 'Z')
-                    {
-                        lowerCaseWordBuilder[charIndex] = (char)(word[charIndex] + 32);
-                    }
-                    else
-                    {
-                        lowerCaseWordBuilder[charIndex] = word[charIndex];
-                    }
+                distinctWords = newArray;
+            }
 
-                    charIndex++;
+            int charIndex = 0;
+            StringBuilder lowerCaseWordBuilder = new()
+            {
+                Length = word.Length
+            };
+        forWordStatement:
+            if (word[charIndex] >= 'A' && word[charIndex] <= 'Z')
+            {
+                lowerCaseWordBuilder[charIndex] = (char)(word[charIndex] + 32);
+            }
+            else
+            {
+                lowerCaseWordBuilder[charIndex] = word[charIndex];
+            }
 
-                    if (charIndex < word.Length)
-                    {
-                        goto forWordStatement;
-                    }
+            charIndex++;
 
-                string lowerCaseWord = lowerCaseWordBuilder.ToString();
+            if (charIndex < word.Length)
+            {
+                goto forWordStatement;
+            }
 
-                int banWordIndex = 0;
-                forBanWordCheckStatement:
-                    if (lowerCaseWord == BannedWords[banWordIndex])
-                    {
-                        index++;
-                        goto forStatement;
-                    }
+            string lowerCaseWord = lowerCaseWordBuilder.ToString();
 
-                    banWordIndex++;
-                    if (banWordIndex < BannedWords.Length)
-                    {
-                        goto forBanWordCheckStatement;
-                    }
+            int banWordIndex = 0;
+        forBanWordCheckStatement:
+            if (lowerCaseWord == BannedWords[banWordIndex])
+            {
+                index++;
+                goto forStatement;
+            }
 
-                int findIndex = 0;
-                forFindElementStatement:
-                    if (distinctWords[findIndex].Item1 == lowerCaseWord)
-                    {
-                        distinctWords[findIndex].Item2++;
-                        goto forFindElementStatementEnd;
-                    }
+            banWordIndex++;
+            if (banWordIndex < BannedWords.Length)
+            {
+                goto forBanWordCheckStatement;
+            }
 
-                    findIndex++;
-                    if (findIndex < elementsCount)
-                    {
-                        goto forFindElementStatement;
-                    }
+            int findIndex = 0;
+        forFindElementStatement:
+            if (distinctWords[findIndex].Item1 == lowerCaseWord)
+            {
+                distinctWords[findIndex].Item2++;
+                goto forFindElementStatementEnd;
+            }
 
-                    distinctWords[elementsCount++] = (lowerCaseWord, 1);
+            findIndex++;
+            if (findIndex < elementsCount)
+            {
+                goto forFindElementStatement;
+            }
 
-                forFindElementStatementEnd:
-                    index++;
+            distinctWords[elementsCount++] = (lowerCaseWord, 1);
 
-                if (index < data.Length)
-                {
-                    goto forStatement;
-                }
+        forFindElementStatementEnd:
+            index++;
 
-            forStatementEnd:
+            if (index < data.Length)
+            {
+                goto forStatement;
+            }
+
+        forStatementEnd:
             int i = 0;
-            forSortStatement:
-                int j = 0;
-                forSortSecondStatement:
-                    if (distinctWords[j].Item2 < distinctWords[j + 1].Item2)
-                    {
-                        (distinctWords[j], distinctWords[j + 1]) = (distinctWords[j + 1], distinctWords[j]);
-                    }
+        forSortStatement:
+            int j = 0;
+        forSortSecondStatement:
+            if (distinctWords[j].Item2 < distinctWords[j + 1].Item2)
+            {
+                (distinctWords[j], distinctWords[j + 1]) = (distinctWords[j + 1], distinctWords[j]);
+            }
 
-                    j++;
-                    if (j < distinctWords.Length - 1)
-                    {
-                        goto forSortSecondStatement;
-                    }
+            j++;
+            if (j < distinctWords.Length - 1)
+            {
+                goto forSortSecondStatement;
+            }
 
-                i++;
-                if (i < distinctWords.Length)
-                {
-                    goto forSortStatement;
-                }
+            i++;
+            if (i < distinctWords.Length)
+            {
+                goto forSortStatement;
+            }
 
             int elementIndex = 0;
             using StreamWriter writer = new("output.txt");
-            forOutputStatement:
-                if (distinctWords[elementIndex].Item2 != 0)
-                {
-                    writer.WriteLine($"{distinctWords[elementIndex].Item1} - {distinctWords[elementIndex].Item2}");
-                }
+        forOutputStatement:
+            if (distinctWords[elementIndex].Item2 != 0)
+            {
+                writer.WriteLine($"{distinctWords[elementIndex].Item1} - {distinctWords[elementIndex].Item2}");
+            }
 
-                elementIndex++;
-                if (elementIndex < distinctWords.Length)
-                {
-                    goto forOutputStatement;
-                }
+            elementIndex++;
+            if (elementIndex < distinctWords.Length)
+            {
+                goto forOutputStatement;
+            }
         }
     }
 }
